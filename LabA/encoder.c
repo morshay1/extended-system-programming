e34c42d130a664219690b5f766b6e74c2a697ffb
Mor Shay <morshay99@gmail.com>
2026-06-25 10:49:43 +0300

    started rewriting encoder

--- LabA/encoder.c
+++ LabA/encoder.c
@@ -2,10 +2,16 @@
 
 int main(int argc, char *argv[])
 {
-    
-    int c;
-    while ((c = fgetc(stdin)) != '\n') {
-        printf("Read character: %c\n", c);
+    // Debug Mode
+    for (int i = 0; i < argc; i++) {
+        if (argv[1][0] == '+' && argv[1][1] == 'D') {
+            printf(argv[i])
+        }
     }
+
+    // int c;
+    // while ((c = fgetc(stdin)) != '\n') {
+    //     printf("Read character: %c\n", c);
+    // }
     return 0;
 }
